a6affb8bfc891a5fcca7d231bd16659264c5e962
DeD_off <dmi.workbox@gmail.com>
2019-01-30 00:35:57 +0400

    work

--- Slider/main.js
+++ Slider/main.js
@@ -1,96 +1,92 @@
 var imgs = [
     "1.JPG","2.JPG","3.JPG","4.JPG","5.JPG"
 ]
-
-
+var showItemsCount =2; 
+var fragments = []; 
 
 var Counter = (function(){
-    var totalItemsCount = imgs.length;
-    var currentIndex = 0;
+    let totalItemsCount = imgs.length; 
+    let currentIndex = 0; 
+    let lastMoveToRight = true; 
 
-    return {
-        getCurrent(){
-            return currentIndex;
-        },
-
-        next(){
-            return (currentIndex + 1) % totalItemsCount;
-        },
-        previous(){
-            let i = currentIndex -1;
-            if(i < 0){
-                i = totalItemsCount + i;
-            }
-            return i % totalItemsCount;
-        },
-        add(){
-            let i = currentIndex +1;
-            currentIndex = i %totalItemsCount;
-            return currentIndex;
-        },
-        minus(){
-            let i = currentIndex -1;
-            if(i < 0){
-                i = totalItemsCount + i;
-            }
-            currentIndex = i %totalItemsCount;
-            return currentIndex;
-        }
-        ,set(index){
-            if(index < 0){
-                index = Math.abs(index);
-            }
-            currentIndex = index  % totalItemsCount;
-        }
-
-
-    }
-})();
+    return { 
+        getCurrent(){ 
+            return currentIndex; 
+        }, 
+        add(){ 
+            if(lastMoveToRight){ 
+                currentIndex = currentIndex +1; 
+            } 
+            else{ 
+                currentIndex = currentIndex + showItemsCount; 
+            } 
+                lastMoveToRight = true; 
+        }, 
+        minus(){ 
+            if(lastMoveToRight){ 
+                currentIndex = currentIndex - showItemsCount; 
+            } 
+            else{ 
+                currentIndex = currentIndex -1; 
+            } 
+            lastMoveToRight = false; 
+        } 
+        ,set(index){ 
+            currentIndex = index; 
+        }, 
+        imgIndex(index){ 
+            if(index >= 0) 
+                return index % totalItemsCount; 
+            
+            return totalItemsCount - (Math.abs(index)% totalItemsCount) -1 
+        } 
+    } 
+})(); 
+     
+function ifSlideExist(slideId){ 
+    var fragment = fragments.find((el, index, array)=> { 
+    if(el.id == slideId) 
+        return true; 
+    }); 
+    
+    return fragment?true:false; 
+} 
 
 function render(){
-    let image = getImgElement('#mainSlide');
-    let leftimage = getImgElement('#leftSlide');
-    let rightimage = getImgElement('#rightSlide');
-    image.src = imgs[Counter.getCurrent()];
-    leftimage.src = imgs[Counter.previous()];
-    rightimage.src = imgs[Counter.next()];
-
-    // leftimage.style.display = "none";
-    // rightimage.style.display = "none";
+    var currentIndex = Counter.getCurrent(); 
+    var fragment= fragments.find((el, index, array)=> { 
+            if(el.id == currentIndex) 
+                return el; 
+    }); 
+        
+    var slide = fragment.element.querySelector('.slide'); 
+    slide.scrollIntoView({behavior: "smooth"}); 
 }
 
-function getImgElement(id){
-    let retElement = null;
-    document.querySelector(id).childNodes
-    .forEach(element => {
-        if(element instanceof HTMLImageElement)
-            retElement= element;
-    });
-    return retElement;
-}
+function createSlide(slideId){ 
+    const fragment = document.createElement('fragment'); 
+    
+    fragment.innerHTML += ` 
+    <span class="slide"> 
+        <img class="slide--img" src=${imgs[Counter.imgIndex(slideId)]}> 
+    </span> 
+    ` 
+    
+    let it = {id:slideId, element : fragment}; 
+    fragments.push(it); 
+    return fragment; 
+} 
 
 
 (function init(){
-    const fragment = document.createElement('fragment');
-    fragment.innerHTML = `
-            <div class="main-container">
-                <span class="slide" id="leftSlide">
-                    <img class="slide--img">
-                </span>
-                <span class="slide" id="mainSlide">
-                    <img class="slide--img">
-                    </span>
-                <span class="slide" id="rightSlide">
-                    <img class="slide--img">
-                </span>
-            </div>
-
-            <div style="display:inline;">
-                <span>
-                    <button type="button" id="btnLeft">&larr;</button>
-                </span>
-               
-            `
+    const fragment = document.createElement('fragment'); 
+    fragment.innerHTML = ` 
+       
+        <div class="buttons"> 
+        <span> 
+            <button type="button" id="btnLeft">&larr;</button> 
+        </span> 
+    ` 
 
     for(var i =0; i< imgs.length; i++){
         fragment.innerHTML += `
@@ -109,37 +105,65 @@ function getImgElement(id){
 
     document.querySelector('body').appendChild(fragment);
 
-    let image = getImgElement('#mainSlide');
-    image.style.border = '5px solid red';
-    image.classList.add('slide__active__img');
+    let container = document.querySelector('.main-container'); 
 
-    var btnl = document.getElementById("btnLeft");  
-
-    btnl.addEventListener('click', (e)=>{
-        Counter.minus();
-        render();
-    });
-
-    var btnr = document.getElementById('btnRight');
-    btnr.addEventListener('click', (e)=>{
-        Counter.add();
-        render();
-    });
-
-    var btns =  Array.from(document.querySelectorAll('[name="slidersButtons[]"]'));
-    btns.forEach((btn)=>{
-        btn.addEventListener('click', (e)=>{
-            Counter.set(btn.value);
-            render();
-        });
+    Counter.set(0); 
+    container.appendChild(createSlide(Counter.getCurrent())); 
+    Counter.add(); 
+    container.appendChild(createSlide(Counter.getCurrent())); 
+    Counter.add(); 
+    container.appendChild(createSlide(Counter.getCurrent())); 
+    Counter.add(); 
+    container.appendChild(createSlide(Counter.getCurrent())); 
+    Counter.add(); 
+    container.appendChild(createSlide(Counter.getCurrent())); 
     
-    });
+
+    //toLeft 
+    var btnl = document.getElementById("btnLeft"); 
+    btnl.addEventListener('click', (e)=>{ 
+        Counter.minus(); 
+        var slideId = Counter.getCurrent(); 
+        if(!ifSlideExist(slideId)){ 
+            var slide = createSlide(slideId); 
+            var container = document.querySelector('.main-container'); 
+            container.insertBefore(slide, container.firstChild); 
+        } 
+        render(); 
+        
+    }); 
 
 
+    //toRight 
+    var btnr = document.getElementById('btnRight'); 
+    btnr.addEventListener('click', (e)=>{ 
+        Counter.add(); 
+        var slideId = Counter.getCurrent(); 
+        if(!ifSlideExist(slideId)){ 
+            var slide = createSlide(slideId); 
+            document.querySelector('.main-container').appendChild(slide); 
+        } 
+        render(); 
+    }); 
 
-    render();
-})();
+// var btns = Array.from(document.querySelectorAll('[name="slidersButtons[]"]'));
+// btns.forEach((btn)=>{ 
+// btn.addEventListener('click', (e)=>{ 
+// Counter.set(btn.value); 
+// render(); 
+// }); 
 
+// }); 
+
+})(); 
+
+document.addEventListener('DOMContentLoaded', function(){ // Аналог $(document).ready(function(){
+    var el = fragments[3];
+    
+    var slide = el.element.querySelector('.slide'); 
+    slide.scrollIntoView({behavior: "smooth"}); 
+    
+});
 
 
 
